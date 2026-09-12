46a4bd310a1da49ab1476875dc53ca8bcdd8e20e
ewquy <isimsoyisim3605@gmail.com>
2026-09-12 22:23:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const { Client, CustomStatus, RichPresence } = require('discord.js-selfbot-v13');
+const { Client } = require('discord.js-selfbot-v13');
 const { joinVoiceChannel } = require('@discordjs/voice');
 const http = require('http');
 
-// Render'ın kapanmaması için web sunucusu
-http.createServer((req, res) => res.end('7/24 Aktif')).listen(process.env.PORT || 3000);
+// Sunucunun kapanmaması için küçük web sunucusu
+http.createServer((req, res) => res.end('7/24 AFK Aktif')).listen(process.env.PORT || 3000);
 
 const client = new Client({ checkUpdate: false });
 
@@ -12,34 +12,14 @@ const GUILD_ID = process.env.GUILD_ID;
 const VOICE_CHANNEL_ID = process.env.VOICE_CHANNEL_ID;
 
 client.on('ready', async () => {
-    console.log(`${client.user.tag} bulutta aktif!`);
+    console.log(`${client.user.tag} olarak seste AFK moduna geçildi!`);
 
-    // --- SPOTIFY DİNLİYOR AYARLARI ---
-    const songName = 'kazakistan'';
-    const artistName = 'irlanda';
-    const albumName = '363';
-
-    const spotifyActivity = new RichPresence(client)
-        .setApplicationId('1101928302322303030')
-        .setName('Spotify')
-        .setType('LISTENING')
-        .setDetails(songName)
-        .setState(artistName)
-        .setAssetsLargeImage('spotify:ab67616d0000b273610e20601f01633519808a54')
-        .setAssetsLargeText(albumName)
-        .setStartTimestamp(Date.now());
-
-    // --- ÖZEL DURUM YAZISI ---
-    const customStatus = new CustomStatus(client)
-        .setState('/363');
-
-    // --- HESAP DURUMU (RAHATSIZ ETMEYİN) ---
+    // Profildeki oynuyor/dinliyor durumunu temizler ve doğrudan varsayılan moda alır
     client.user.setPresence({
-        activities: [spotifyActivity, customStatus],
-        status: 'dnd', // dnd = Rahatsız Etmeyin (Kırmızı İkon)
+        activities: [],
+        status: 'dnd', // dnd = Rahatsız Etmeyin (kırmızı ikon). İstersen 'online' yapabilirsin.
     });
 
-    // --- SESE BAĞLANMA ---
     try {
         const guild = await client.guilds.fetch(GUILD_ID);
         const channel = await guild.channels.fetch(VOICE_CHANNEL_ID);
@@ -48,8 +28,8 @@ client.on('ready', async () => {
             channelId: channel.id,
             guildId: guild.id,
             adapterCreator: guild.voiceAdapterCreator,
-            selfDeaf: true,
-            selfMute: true,
+            selfDeaf: true, // Sağırlaştırılmış (kulaklık kapalı)
+            selfMute: true, // Susturulmuş (mikrofon kapalı)
         });
 
         console.log(`Başarıyla ${channel.name} ses kanalına girildi!`);
